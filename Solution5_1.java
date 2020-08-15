447a568dbc71f7890069519b01770aacbb6d38eb
guirunxiang <1028713883@qq.com>
2020-08-15 20:58:40 +0800

    最长回文子串 中心扩展解法

--- Solution5_1.java
+++ Solution5_1.java
@@ -1,3 +1,24 @@
 /**
- * 
+ * 5.最长回文子串
  */
+ //中心扩散解法
+ class Solution5_1 {
+     public String longestPalindrome(String s) {
+        String res = "";
+        for(int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i+1);
+            res = res.length() > s1.length() ? res : s1;
+            res = res.length() > s2.length() ? res : s2;
+        }
+        return res;
+     }
+
+     private String palindrome(String s, int left, int right) {
+         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+             left--;
+             right++;
+         }
+         return s.substring(left+1, right);
+     }
+ }
